c764534073749d56a63206506d839a41105d4f2c
Annmariya3820 <mariyadaisy320@gmail.com>
2024-06-13 09:16:40 +0530

    completed

--- src/components/View.jsx
+++ src/components/View.jsx
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
+import Navbar from './Navbar'
 
 const View = () => {
     const [data,setData]=useState([])
@@ -18,6 +19,7 @@ const View = () => {
     useEffect(()=>{fetchData()},[])
   return (
     <div>
+        <Navbar/>
        <div className="container">
         <div className="row">
             <div className="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
